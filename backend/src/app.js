3b2deddb3e21ca9c0fc366dc3fdef7991e635df6
Mallikarjun <mallikarjunharsoor28@gmail.com>
2026-05-13 22:57:26 +0530

    fifth commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,10 @@ const cookie = require('cookie-parser');
 const router = require('./routes/auth');
 const postAuth = require('./routes/postAuth');
 const userRout = require('./routes/user.auth');
+const cors = require('cors');
+
 const app = express();
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookie());
 app.use('/auth', router);
